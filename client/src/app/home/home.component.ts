bcbcdf21646d0f7a99c0541d40328f87114aa253
markj <sc.markj0307@gmail.com>
2024-03-13 23:21:32 +0800

    initialize commit

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products, Product } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator'
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ProductComponent, CommonModule, PaginatorModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  constructor(private productsService: ProductsService) {}
+
+  products: Product[] = [];
+
+  totalRecords: number = 0;
+  rows: number = 5;
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+
+  onPageChange(event: any){
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number){
+    this.productsService
+    .getProducts('http://localhost:3000/clothes', { page, perPage })
+    .subscribe((products: Products) => {
+      this.products = products.items;
+      this.totalRecords = products.total;
+    });
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0,this.rows)
+  }
+}
